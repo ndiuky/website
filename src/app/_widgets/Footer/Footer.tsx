90a07dd5cdad7f30271ffacf366609f2733c6d9f
Merlin Sparrow <anod100002@gmail.com>
2026-04-04 22:51:49 +0400

    абоба

--- src/app/_widgets/Footer/Footer.tsx
+++ src/app/_widgets/Footer/Footer.tsx
@@ -4,8 +4,8 @@ export const Footer = async () => {
 
   return (
     <footer className="bottom-0 w-full bg-slate-100 dark:bg-black flex items-center justify-between py-3 px-6">
-      <a href={data.prev.url}>{data.prev.name}</a>
-      <a href={data.next.url}>{data.next.name}</a>
+      <a href="https://webring.otomir23.me/39/prev">{"<-"}</a>
+      <a href="https://webring.otomir23.me/39/next">{"->"}</a>
     </footer>
   );
 };
